554947f3ae0491cb6ee045a543dd4742d0d29bda
Rafael Costa <zrafaelcostaz@gmail.com>
2024-11-21 16:35:48 -0300

    Initial Exemplo 1

---
+++ FOR/src/Exemplo1.java
@@ -0,0 +1,13 @@
+import java.util.Locale;
+import java.util.Scanner;;
+
+public class Exemplo1 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        sc.close();
+
+    }
+}
