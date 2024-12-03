550c14d0c82fdc61dcdec5e4317ee3c8ec106acd
Rafael Costa <zrafaelcostaz@gmail.com>
2024-12-03 10:49:39 -0300

    Exemplo Operação Slip

---
+++ FOR/src/OperacaoSlip01.java
@@ -0,0 +1,14 @@
+public class OperacaoSlip01 {
+ public static void main(String[] args) {
+
+    String s = "batata maçã limão";
+
+    String[] vect = s.split(" ");
+
+    System.out.println(vect[0]);
+    System.out.println(vect[1]);
+    System.err.println(vect[2]);
+
+ }
+    
+}
